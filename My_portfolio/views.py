1e7a73dea2895cd1240fda624beb46c77e4f1bbd
chandan <cs870755@gmail.com>
2023-03-01 16:53:43 +0530

    all static and templates files added and creation of function in views

--- My_portfolio/views.py
+++ My_portfolio/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from.models import *
 
 # Create your views here.
+def home(request):
+    return render(request,'base.html')
+def aboutus(request):
+    return render(request,'about.html')
+def my_work(request):
+    return render(request,'work.html')
+def blog(request,bid):
+    return render(request,'blog.html')
+def contact(request):
+    return render(request,'contact.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from My_portfolio.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('',home),
+    path('work/',my_work,name='work'),
 ]
